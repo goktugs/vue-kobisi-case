9a4babcbdbb88c0a00ceb030d923c2d3d8e9eeac
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-07-21 02:06:04 +0300

    Add AppCard

---
+++ src/components/Card/AppCard.vue
@@ -0,0 +1,61 @@
+<script>
+import productData from '@/assets/product-list.json';
+import AppCardItem from './AppCardItem.vue'
+
+export default {
+    name: "AppCard",
+    components: {
+        AppCardItem
+    },
+    data() {
+        return {
+            products: productData,
+        };
+    },
+
+};
+</script>
+
+<template>
+
+    <div v-for="product in products" :key="product.id" class="cardContainer">
+        <AppCardItem :image="product.image" :name="product.name" :newPrice="product.newPrice"
+            :oldPrice="product.oldPrice" :id="product.id" />
+    </div>
+
+</template>
+
+<style lang="scss">
+.cardContainer {
+    height: 100%;
+    width: 100%;
+    border: 1px solid black;
+
+    &:nth-of-type(1) {
+        grid-area: 1 / 1 / span 2 / span 2;
+    }
+}
+
+// .smallCard {
+//     display: flex;
+//     align-items: center;
+//     justify-content: center;
+//     flex-direction: column;
+
+//     .productImage {
+//         background-color: white;
+//     }
+
+//     .productSpec {
+//         display: flex;
+//         align-items: center;
+//         justify-content: center;
+
+//         h4 {
+//             font-weight: 500;
+//             font-size: 20px;
+//         }
+//     }
+
+// }
+</style>
